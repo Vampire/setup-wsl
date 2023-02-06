e82aaf507802360df4d4e585efd2e490beb7203c
Björn Kautler <Bjoern@Kautler.net>
2023-02-06 04:09:24 +0100

    Update Gradle enterprise plugin to 3.12.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
 
 plugins {
     id("de.fayard.refreshVersions") version "0.51.0"
-    id("com.gradle.enterprise") version "3.6.1"
+    id("com.gradle.enterprise") version "3.12.3"
 }
 
 refreshVersions {
