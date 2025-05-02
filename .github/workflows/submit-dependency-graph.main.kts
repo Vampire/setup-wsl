c7ed15c966e4bf6953ba3033c82dbc2b7e42ddbd
Björn Kautler <Bjoern@Kautler.net>
2025-05-02 09:39:42 +0200

    Submit the Dependency Graph to GitHub

---
+++ .github/workflows/submit-dependency-graph.main.kts
@@ -0,0 +1,88 @@
+#!/usr/bin/env kotlin
+
+/*
+ * Copyright 2025 Björn Kautler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Import("workflow-with-copyright.main.kts")
+
+@file:Repository("https://repo.maven.apache.org/maven2/")
+// work-around for https://youtrack.jetbrains.com/issue/KT-69145
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.2.0")
+
+@file:Repository("https://bindings.krzeminski.it/")
+@file:DependsOn("actions:checkout___major:[v4,v5-alpha)")
+@file:DependsOn("actions:setup-java___major:[v4,v5-alpha)")
+@file:DependsOn("gradle:actions__dependency-submission___major:[v4,v5-alpha)")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.actions.actions.SetupJava
+import io.github.typesafegithub.workflows.actions.actions.SetupJava.Distribution.Temurin
+import io.github.typesafegithub.workflows.actions.gradle.ActionsDependencySubmission
+import io.github.typesafegithub.workflows.actions.gradle.ActionsDependencySubmission.BuildScanTermsOfUseAgree.Yes
+import io.github.typesafegithub.workflows.actions.gradle.ActionsDependencySubmission.BuildScanTermsOfUseUrl.HttpsGradleComHelpLegalTermsOfUse
+import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+
+// comment in for editability with IntelliSense
+//fun workflowWithCopyright(
+//    name: String,
+//    on: List<io.github.typesafegithub.workflows.domain.triggers.Trigger>,
+//    sourceFile: java.io.File,
+//    block: io.github.typesafegithub.workflows.dsl.WorkflowBuilder.() -> Unit
+//) = Unit
+
+workflowWithCopyright(
+    name = "Submit Dependency Graph",
+    on = listOf(
+        Push(branches = listOf("master"))
+    ),
+    sourceFile = __FILE__
+) {
+    job(
+        id = "submit_dependency_graph",
+        name = "Submit Dependency Graph",
+        runsOn = UbuntuLatest
+    ) {
+        run(
+            name = "Configure Git",
+            command = "git config --global core.autocrlf input"
+        )
+        uses(
+            name = "Checkout",
+            action = Checkout()
+        )
+        uses(
+            name = "Setup Java 11",
+            action = SetupJava(
+                javaVersion = "11",
+                distribution = Temurin
+            )
+        )
+        uses(
+            name = "Submit Dependency Graph",
+            action = ActionsDependencySubmission(
+                additionalArguments = listOf(
+                    "--info",
+                    "--stacktrace",
+                    "--show-version"
+                ),
+                buildScanPublish = true,
+                buildScanTermsOfUseUrl = HttpsGradleComHelpLegalTermsOfUse,
+                buildScanTermsOfUseAgree = Yes
+            )
+        )
+    }
+}
